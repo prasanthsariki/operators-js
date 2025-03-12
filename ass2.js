3cc97c84d016a166ddff6f0dfc7620b59950ed07
prasanth sariki <prasanthsariki@gmail.com>
2025-03-12 17:52:21 +0530

    add files

---
+++ ass2.js
@@ -0,0 +1,132 @@
+// result of 10 + '5'
+console.log(10+'5')
+// 105
+
+
+// value of x after x=10; x+=5
+let x=10
+
+console.log(x+=5)
+// 15
+
+
+// result of 10 - null
+
+console.log(10 - null)
+// 10
+
+// vaalue of x after x = 10;x* =2
+
+let x1=10
+console.log(x1 *= 2)
+// 20
+
+// result of 10/'2'
+
+console.log(10/'2')
+// 5
+
+// result of 10 % 3
+
+console.log(10%3)
+// 1
+
+// value of x after x = 10 ,x=x**2
+
+let x2 = 10;
+console.log(x2=x2**2)
+// 100
+
+// result of 10 > 5 && 5 < 10
+
+console.log(10 > 5 && 5 < 10)
+// true
+
+// result of 10==='10'
+console.log(10 === '10')
+// false
+
+// result of  null ?? 'default'
+console.log(null ?? 'default')
+
+// default
+
+// result of  10 || 'default'
+console.log( 10 || 'default')
+// 10
+
+// result of !true
+console.log(!true)
+// false
+
+// result of 10 & 5
+console.log(10 & 5)
+// 0
+
+// result of 10 | 5
+console.log(10 | 5)
+// 15
+
+// result of 10 ^ 5
+console.log(10 ^ 5)
+
+// x =10 and y = ++x
+
+let x5 =10
+console.log(x5)
+let y = ++x5
+
+console.log(y)
+// 11,11
+
+// x =10 and y = x++
+let x6 =10
+console.log(x6)
+let y1 = x6++
+
+console.log(y1)
+// 11,10
+
+// reslut of x =10;
+
+let x7 = 10
+console.log(++x7)
+// 11
+
+let x8 = 10
+console.log(x8++)
+// 10
+
+
+// res of x=10 y = -x
+
+let x9 =10
+console.log(x9)     
+// 10
+let y3 = --x9
+console.log(y3)
+// 9
+
+
+// res of x=10 y = x-
+
+let x11 =10
+console.log(x11)     
+// 10
+let y4 = x11--
+console.log(y4)
+// 10
+
+// ternary operators
+// what is the result of x=10; y=x>5?'yes':'no';?
+
+let x12=10;
+let y5 = x>5?'yes':'no';
+console.log(y5)
+// yes
+
+// res of x=10;y=x>5?'yes':x>3?'may be':'no'
+
+let x13= 10;
+let y7= x>5?'yes':x>3?'maybe':'no';
+console.log(y7)
